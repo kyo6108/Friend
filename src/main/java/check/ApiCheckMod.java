4f5b2add9d2da650824f32874f0ccf2e5e70552c
Harvey <kyo6108@gmail.com>
2023-04-17 22:16:28 +0800

    增加依賴

--- src/main/java/check/ApiCheckMod.java
+++ src/main/java/check/ApiCheckMod.java
@@ -1,9 +1,11 @@
 package check;
 
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
 import vo.ApiResultVo;
 
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -11,7 +13,11 @@ public class ApiCheckMod {
     public void checkInitParameter(ApiResultVo apiResultVo) {
         String method = apiResultVo.getRequestMethod();
         String url = apiResultVo.getRequestUrl();
+        List<String> errMsgList;
         Map<String,Object> requestBody = apiResultVo.getRequestMap();
+        if (StringUtils.isBlank(method)){
+            
+        }
 
 
 
